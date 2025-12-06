ac5d9e0520b1c3fe2ef28890db1afe748aa5f9ca
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2025-12-06 13:26:39 +0530

    feat(viewmodel): add NotefulViewModel with note CRUD and search support

---
+++ app/src/main/java/com/prince/noteful/ui/viewModels/NotefulViewModel.kt
@@ -0,0 +1,43 @@
+package com.prince.noteful.ui.viewModels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.prince.noteful.data.local.NoteEntity
+import com.prince.noteful.data.repo.NotesRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NotefulViewModel @Inject constructor(
+    private val repo: NotesRepository
+): ViewModel() {
+
+    val notes = repo.notes.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = emptyList()
+    )
+
+
+    private val _activeNote = MutableStateFlow<NoteEntity?>(null)
+    val activeNote: StateFlow<NoteEntity?> = _activeNote
+
+    fun loadNote(id:String) = viewModelScope.launch {
+        _activeNote.value = repo.getNoteById(id)
+    }
+
+    fun saveNote(note: NoteEntity) = viewModelScope.launch {
+        repo.upsertNote(note)
+    }
+
+    fun deleteNote(note: NoteEntity) = viewModelScope.launch {
+        repo.deleteNote(note)
+    }
+
+    suspend fun searchNote(query:String) = repo.searchNote(query)
+}
