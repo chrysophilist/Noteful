303c54e13430e70d0360746f9bb9a34c2c8503ca
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2025-12-07 23:29:26 +0530

    feat(ui): add ActiveNoteScreen

---
+++ app/src/main/java/com/prince/noteful/ui/screens/ActiveNoteScreen.kt
@@ -0,0 +1,152 @@
+package com.prince.noteful.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Archive
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.NotificationAdd
+import androidx.compose.material.icons.filled.PushPin
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.prince.noteful.data.local.NoteEntity
+import com.prince.noteful.ui.viewModels.NotefulViewModel
+import java.util.UUID
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ActiveNoteScreen(
+    viewModel: NotefulViewModel,
+    onSave: ()-> Unit
+) {
+    val activeNote by viewModel.activeNote.collectAsState()
+    val existingTitle = activeNote?.title.orEmpty()
+    val existingContent = activeNote?.content.orEmpty()
+
+    var titleInput by rememberSaveable { mutableStateOf(activeNote?.title ?: "") }
+    var contentInput by rememberSaveable { mutableStateOf(activeNote?.content ?: "") }
+    val hasModified = titleInput != existingTitle || contentInput != existingContent
+
+    val scrollState = rememberScrollState()
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {},
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            val note = NoteEntity(
+                                id = activeNote?.id ?: UUID.randomUUID().toString(),
+                                title = titleInput.trim(),
+                                content = contentInput.trim()
+                            )
+                            viewModel.saveNote(note)
+                            onSave()
+                        }
+                    ) {
+                        Icon(
+                            Icons.AutoMirrored.Filled.ArrowBack,
+                            "Return to home screen"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {}) { Icon(Icons.Default.PushPin, "Pin") }
+                    IconButton(onClick = {}) { Icon(Icons.Default.NotificationAdd, "Reminder") }
+                    IconButton(onClick = {}) { Icon(Icons.Default.Archive, "Archive") }
+                    IconButton(onClick = {}) { Icon(Icons.Default.MoreVert, "More") }
+                }
+            )
+        }
+    ) { innerPadding-> 
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top
+        ){
+            TextField(
+                value = titleInput,
+                onValueChange = { titleInput=it },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(),
+                textStyle = MaterialTheme.typography.titleLarge,
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(12.dp)
+            )
+
+            Spacer(Modifier.height(8.dp))
+
+            TextField(
+                value = contentInput,
+                onValueChange = { contentInput=it },
+                singleLine = false,
+                maxLines = Int.MAX_VALUE,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+                    .verticalScroll(scrollState),
+                textStyle = MaterialTheme.typography.bodyLarge,
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(12.dp)
+            )
+
+            Spacer(Modifier.height(8.dp))
+
+            if (hasModified){
+                Button(
+                    onClick = {
+                        val note = NoteEntity(
+                            id = activeNote?.id ?: UUID.randomUUID().toString(),
+                            title = titleInput.trim(),
+                            content = contentInput.trim()
+                        )
+                        viewModel.saveNote(note)
+                        onSave()
+                    },
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                ) {
+                    Text("Save Note")
+                }
+            }
+        }
+    }
+}
