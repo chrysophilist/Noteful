752d94d66c479307e13da6b62b6a3a6ac003dbd5
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2025-12-08 06:18:57 +0530

    feat: implement search bar and note filtering logic

--- app/src/main/java/com/prince/noteful/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/prince/noteful/ui/screens/HomeScreen.kt
@@ -12,8 +12,10 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.DeleteForever
 import androidx.compose.material.icons.filled.GridView
 import androidx.compose.material.icons.filled.Menu
@@ -22,14 +24,18 @@ import androidx.compose.material.icons.filled.ViewAgenda
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -41,6 +47,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.prince.noteful.ui.viewModels.NotefulViewModel
 
@@ -52,6 +59,9 @@ fun HomeScreen(
 ) {
     var isNoteSheetOpen by rememberSaveable() { mutableStateOf(false) }
     var isGrid by rememberSaveable { mutableStateOf(true) }
+
+    var isSearchBarActive by rememberSaveable { mutableStateOf(false) }
+    var query by rememberSaveable { mutableStateOf("") }
 
     if(isNoteSheetOpen){
         ModalBottomSheet(
@@ -72,37 +82,85 @@ fun HomeScreen(
         modifier = Modifier
             .fillMaxSize(),
         topBar = {
-            TopAppBar(
-                title = { Text("Noteful") },
-                navigationIcon = {
-                    IconButton( onClick = {} ) {
-                        Icon(
-                            imageVector = Icons.Default.Menu,
-                            contentDescription = "Menu"
+            if (isSearchBarActive){
+                TopAppBar(
+                    title = {
+                        TextField(
+                            value = query,
+                            onValueChange = {
+                                query=it
+                            },
+                            placeholder = {Text("Search notes...")},
+                            modifier = Modifier.fillMaxWidth(),
+                            singleLine = true,
+                            colors = TextFieldDefaults.colors(
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                                unfocusedContainerColor = Color.Transparent,
+                                focusedContainerColor = Color.Transparent
+                            )
                         )
+                    },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                                isSearchBarActive = false
+                                query = ""
+                            }
+                        ) { Icon(Icons.AutoMirrored.Filled.ArrowBack, "Close Search")}
+                    },
+                    actions = {
+                        if (query.isNotBlank()){
+                            IconButton(
+                                onClick = { query = "" }
+                            ) {
+                                Icon(Icons.Default.Clear, "Clear Search")
+                            }
+                        }
                     }
-                },
-                actions = {
-                    IconButton(onClick = {}) { Icon(Icons.Default.Search, "Search") }
-                    if (isGrid){
-                        IconButton(onClick = { isGrid = false}) { Icon(Icons.Default.ViewAgenda, "Single-column view") }
-                    } else {
-                        IconButton(onClick = { isGrid = true }) { Icon(Icons.Default.GridView, "Multi-column view") }
+                )
+            } else {
+                TopAppBar(
+                    title = { Text("Noteful") },
+                    navigationIcon = {
+                        IconButton( onClick = {} ) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = "Menu"
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { isSearchBarActive=true }) { Icon(Icons.Default.Search, "Search") }
+                        if (isGrid){
+                            IconButton(onClick = { isGrid = false}) { Icon(Icons.Default.ViewAgenda, "Single-column view") }
+                        } else {
+                            IconButton(onClick = { isGrid = true }) { Icon(Icons.Default.GridView, "Multi-column view") }
+                        }
+                        IconButton(onClick = {}) { Icon(Icons.Default.AccountCircle, "Account") }
                     }
-                    IconButton(onClick = {}) { Icon(Icons.Default.AccountCircle, "Account") }
-                }
-            )
+                )
+            }
         },
         floatingActionButton = {
-            FloatingActionButton(
-                onClick = { isNoteSheetOpen = true }
-            ) {
-                Icon(Icons.Default.Add, "Add Note")
-            }
+            ExtendedFloatingActionButton(
+                onClick = { isNoteSheetOpen = true },
+                icon = { Icon(Icons.Default.Add, "Add Note") },
+                text = {Text("Add Note")},
+                elevation = FloatingActionButtonDefaults.elevation(defaultElevation = 20.dp)
+            )
         }
     ) { innerPadding->
 
         val notes by viewModel.notes.collectAsState(emptyList())
+
+        val searchedNotes = if(query.isEmpty()){
+            notes
+        } else {
+            notes.filter { note ->
+                note.title.contains(query, ignoreCase = true) || note.content.contains(query, ignoreCase = true)
+            }
+        }
 
         if (notes.isEmpty()){
             Box(
@@ -130,7 +188,7 @@ fun HomeScreen(
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                items(notes){ note->
+                items(searchedNotes){ note->
                     NoteCard(
                         title = note.title,
                         content = note.content,
@@ -157,12 +215,13 @@ fun NoteCard(
 
     Box {
         OutlinedCard(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
                 .combinedClickable(
-                enabled = true,
-                onClick = { onClick() },
-                onLongClick = { showMenu = true }
-            )
+                    enabled = true,
+                    onClick = { onClick() },
+                    onLongClick = { showMenu = true }
+                )
         ) {
             Column(
                 modifier = Modifier
